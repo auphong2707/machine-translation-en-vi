1295355200421d3bba465f16fb3150aa7a7a348e
Veil <65228307+auphong2707@users.noreply.github.com>
2024-10-30 13:44:10 +0700

    Create kaggle_main.py

---
+++ kaggle_main.py
@@ -0,0 +1,37 @@
+import torch
+from helper import set_seed
+set_seed()
+
+from config import DEVICE
+from data.dataloader import get_dataloader
+from models.seq2seq import Seq2SeqGRU
+from trainer import Seq2SeqTrainer
+from config import BATCH_SIZE, TRAIN_DATA_DIR, VAL_DATA_DIR, TEST_DATA_DIR
+from helper import compute_bleu
+
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning, module="torch")
+
+def main():
+    # Set name of experiment
+    experiment_name = 'experiment_0'
+    
+    # Load data
+    input_lang, output_lang, (train_loader, val_loader, test_loader) = \
+        get_dataloader(dirs=[TRAIN_DATA_DIR, VAL_DATA_DIR, TEST_DATA_DIR], batch_size=BATCH_SIZE)
+    
+    input_lang.save('results/'+experiment_name+'/input_lang.pkl')
+    output_lang.save('results/'+experiment_name+'/output_lang.pkl')
+    
+    # Initialize model
+    model = torch.nn.DataParallel(model, device_ids = [0,1]).to(DEVICE)
+
+    # Initialize trainer
+    trainer = Seq2SeqTrainer(model, experiment_name)
+
+    # Train the model
+    trainer.train(train_loader, val_loader, n_epochs=5, print_every=1, plot_every=1)
+
+if __name__ == "__main__":
+    main()
