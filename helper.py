fc169819ce5fcc3128418ba65674f00ab6b6df2a
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-02 17:44:36 +0700

    Update main

--- helper.py
+++ helper.py
@@ -207,7 +207,7 @@ def preprocess_data_csv(dir: str) -> list:
     """
     data = pd.read_csv(dir, encoding='utf-8')
     data = data.iloc[:, :2]
-    data = data.applymap(lambda x: normalize_string(x) if isinstance(x, str) else x)
+    data = data.map(lambda x: normalize_string(x) if isinstance(x, str) else x)
     data.columns = data.columns.str.strip()
     data.columns = data.columns.str.split().str.join('_')
     pairs = data.values.tolist()
