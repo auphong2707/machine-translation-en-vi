35233fcabb30286b733b0e8099af5e2aa3500784
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-08 10:09:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ ENCODER_BIDIRECTIONAL = True        # Use bidirectional encoder
 
 # [TRAINING PARAMETERS]
 BATCH_SIZE = 512                    # Number of samples per batch
-EPOCHS = 20                         # Number of training epochs
+EPOCHS = 5                         # Number of training epochs
 LEARNING_RATE = 0.001               # Initial learning rate
 TEACHER_FORCING_RATIO = 0.5         # Probability of using teacher forcing in decoder (Change this)
 
