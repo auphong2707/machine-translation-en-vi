f25ac834ce44ebae92723d5bce26cc1629869bcb
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-16 21:32:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ TEACHER_FORCING_RATIO = 0.5         # Probability of using teacher forcing in de
 TFM_EMBEDDING_SIZE = 512            # Transformer embedding size
 TFM_DROPOUT_RATE = 0.3              # Transformer dropout rate
 TFM_NUM_LAYERS = 8                  # Number of transformer layers
-TFM_NUM_HEADS = 8                   # Number of attention heads
+TFM_NUM_HEADS = 16                   # Number of attention heads
 TFM_DIM_FEED_FORWARD = 2048          # Dimension of feed forward network
 
 # [TRAINING PARAMETERS]
