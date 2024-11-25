575fb0f674f646a62d3eaab5039b008d272c8a2a
whatsoever025 <149892454+whatsoever025@users.noreply.github.com>
2024-11-25 21:38:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -28,10 +28,10 @@ UNK_TOKEN = 3                       # Unknown token
 # [MODEL ARCHITECTURES PARAMETERS]
 
 # RNN
-RNN_EMBEDDING_SIZE = 128                # Embedding size
-RNN_HIDDEN_SIZE = 128                   # Hidden size
-RNN_NUM_LAYERS = 3                      # Number of RNN layers
-RNN_DROPOUT_RATE = 0.1                  # Dropout rate
+RNN_EMBEDDING_SIZE = 256                # Embedding size
+RNN_HIDDEN_SIZE = 512                   # Hidden size
+RNN_NUM_LAYERS = 2                      # Number of RNN layers
+RNN_DROPOUT_RATE = 0.2                  # Dropout rate
 RNN_ENCODER_BIDIRECTIONAL = True        # Use bidirectional encoder
 
 # RNN with Attention
@@ -44,7 +44,7 @@ RNN_ATTN_ENCODER_BIDIRECTIONAL = True   # Use bidirectional encoder
 
 # [TRAINING PARAMETERS]
 BATCH_SIZE = 128                    # Number of samples per batch
-EPOCHS = 50                         # Number of training epochs
+EPOCHS = 40                         # Number of training epochs
 LEARNING_RATE = 0.001               # Initial learning rate
 TEACHER_FORCING_RATIO = 0.5         # Probability of using teacher forcing in decoder
 
@@ -65,5 +65,5 @@ LOG_INTERVAL = 50                   # Print logs every X batches
 # [ADDITIONAL SETTINGS]
 BEAM_WIDTH = 3                      # Beam width for beam search
 ALPHA = 0.75                        # Length normalization factor for beam search
-RNN_EXPERIMENT_NAME = "experiment_0"
+RNN_EXPERIMENT_NAME = "experiment_0_1"
 RNN_ATTN_EXPERIMENT_NAME = "experiment_1"
