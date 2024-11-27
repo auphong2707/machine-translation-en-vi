85a81d2eb81748a4378cdb87a68f38b7888aecd4
TheSyx <minhquantsy@gmail.com>
2024-11-27 07:38:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ RNN_ATTN_ENCODER_BIDIRECTIONAL = True   # Use bidirectional encoder
 
 # [TRAINING PARAMETERS]
 BATCH_SIZE = 128                    # Number of samples per batch
-EPOCHS = 100                         # Number of training epochs
+EPOCHS = 150                         # Number of training epochs
 LEARNING_RATE = 0.0001              # Initial learning rate
 TEACHER_FORCING_RATIO = 0.5         # Probability of using teacher forcing in decoder
 
