1fcc379d48e0089f20c4ab085543c52623243610
Veil <65228307+auphong2707@users.noreply.github.com>
2024-12-07 20:19:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ RNN_ATTN_ENCODER_BIDIRECTIONAL = True   # Use bidirectional encoder
 # [TRAINING PARAMETERS]
 BATCH_SIZE = 256                    # Number of samples per batch
 EPOCHS = 100                         # Number of training epochs
-LEARNING_RATE = 0.00001               # Initial learning rate
+LEARNING_RATE = 0.00007               # Initial learning rate
 TEACHER_FORCING_RATIO = 0.5         # Probability of using teacher forcing in decoder
 
 
@@ -67,4 +67,4 @@ BEAM_WIDTH = 3                      # Beam width for beam search
 ALPHA = 0.75                        # Length normalization factor for beam search
 RNN_EXPERIMENT_NAME = "experiment_0"
 RNN_ATTN_EXPERIMENT_NAME = "experiment_1"
-TFM_EXPERIMENT_NAME = "experiment_2_0"
+TFM_EXPERIMENT_NAME = "experiment_2_1"
